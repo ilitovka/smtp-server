2b57a42e65694bfa7139d9a9c81ea4112d327e38
Litovka, Ihor EX1 <ilitovka@corevalue.net>
2019-12-04 11:10:02 +0200

    [OCE-39996] Small fixes. Adding logs. Etc. TODO: fix/rework merge ics

--- include/helper/icsCreate.js
+++ include/helper/icsCreate.js
@@ -3,34 +3,35 @@ const log = require('../../libs/log').log;
 const moment = require('moment');
 
 const icsCreate = function (event) {
+    try {
+        const eventMerged = {
+            start: moment(event.start).format('YYYY-M-D-H-m').split("-"),
+            end: moment(event.end).format('YYYY-M-D-H-m').split("-"),
+            status: event.status,
+            organizer: {
+                name: event.organizer.params.CN,
+                email: event.organizer.val.replace('mailto:', '')
+            }
+        };
 
-    const eventMerged = {
-        start: moment(event.start).format('YYYY-M-D-H-m').split("-"),
-        end: moment(event.end).format('YYYY-M-D-H-m').split("-"),
-        status: event.status,
-        organizer: {
-            name: event.organizer.params.CN,
-            email: event.organizer.val.replace('mailto:', '')
-        },
-        'X-ORGID': event.ORGID
-    };
-
-    let attendeens = [];
-    for (let i = 0; i < event.attendee.length; i++) {
-        attendeens.push(
-            {...{email: event.attendee[i].val}, ...event.attendee[i].params}
-        );
-    }
-    eventMerged.attendees = attendeens;
-
-    ics.createEvent(eventMerged, (error, value) => {
-        if (error) {
-            log.info(error)
-            return;
+        let attendees = [];
+        for (let i = 0; i < event.attendee.length; i++) {
+            let attendee = event.attendee[i];
+            attendees.push(
+                {
+                    email:      attendee.val.replace('mailto:', ''),
+                    rsvp:       attendee.params.PARTSTAT,
+                    partstat:   attendee.params.PARTSTAT,
+                    role:       attendee.params.ROLE
+                }
+            );
         }
+        eventMerged.attendees = attendees;
 
-        log.info(value)
-    })
+        return ics.createEvent(eventMerged).value;
+    } catch (e) {
+        return '';
+    }
 };
 
 module.exports = icsCreate;
