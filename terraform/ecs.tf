b87f6abcab5e1db04b08d2ce0df51c99b678184c
Vasyl Petrykanyn <vasyl.petrykanyn@avenga.com>
2020-09-04 17:52:20 +0300

    CI: Complete autoscaling of rsvp mail service via target tracking approach [OCE-56511]

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -6,14 +6,6 @@ resource "aws_ecs_cluster" "app" {
   #capacity_providers = ["FARGATE","FARGATE_SPOT"]
 }
 
-resource "aws_appautoscaling_target" "app_scale_target" {
-  service_namespace  = "ecs"
-  resource_id        = "service/${aws_ecs_cluster.app.name}/${aws_ecs_service.app.name}"
-  scalable_dimension = "ecs:service:DesiredCount"
-  max_capacity       = var.ecs_autoscale_max_instances
-  min_capacity       = var.ecs_autoscale_min_instances
-}
-
 
 data "template_file" "container" {
   template = "${file("task-definition.json")}"
